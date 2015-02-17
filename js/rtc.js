dd81fb5cc9d3a89df5ba7e7ae19c8fce1862aa38
max <great_max@hotmail.it>
2015-02-17 11:28:18 +0100

    added sources

---
+++ js/rtc.js
@@ -0,0 +1,135 @@
+var RTC = function() {
+    
+    var cfg = {"iceServers": [{"url": "stun:23.21.150.121"}]};
+    var opt = {"optional": [{"DtlsSrtpKeyAgreement": true}]};
+
+    this.con = new RTCPeerConnection(cfg, opt);
+    this.dc = null;
+    
+    this.username = "User";
+    this.cipher = new Cipher();
+    
+    var context = this;
+
+    
+    this.con.onicecandidate = function(evt) {
+        if(evt.candidate == null) {
+            $(".output").val(JSON.stringify(context.con.localDescription));
+            //copyToClipboard($(".output")[0].value);
+        }
+    };
+    
+    this.con.onconnection = function() {
+        print("Datachannel connected", "text-info");
+    };
+    
+    this.con.ondatachannel = function(evt) {
+        context.dc = evt.channel || evt;
+        context.dc.onopen = function(evt) {
+            print("Connection estabilished.", "text-info");
+        }
+        context.dc.onmessage = function(evt) {
+            var data = JSON.parse(evt.data);
+            
+            var msg = data.message;
+            print("<b>" + data.user + ": </b>" + context.cipher.decrypt(unescape(msg)), "message");
+            
+            var niv = data.n;
+            context.cipher.riv = unescape(context.cipher.decrypt(unescape(niv)));
+        };
+        context.dc.onclose = function(evt) {
+            print("Disconnected.", "text-info");  
+        };
+    };
+    
+};
+
+RTC.prototype.setup = function() {
+    try {
+        this.dc = this.con.createDataChannel("test", {"reliable": true});
+
+        this.dc.onopen = function(evt) {
+            print("Connection estabilished.", "text-info");
+        }
+        var context = this;
+        this.dc.onmessage = function(evt) {
+            var data = JSON.parse(evt.data);            
+            
+            var msg = data.message;
+            print("<b>" + data.user + ": </b>" + context.cipher.decrypt(unescape(msg)), "message");
+            
+            var niv = data.n;
+            context.cipher.riv = unescape(context.cipher.decrypt(unescape(niv)));
+        };
+        this.dc.onclose = function(evt) {
+            print("Disconnected.", "text-info");  
+        };
+    } catch(e) { 
+            print("Connection error.", "text-err");
+        }
+};
+
+RTC.prototype.createLocalOffer = function() {
+    if(this.dc == null) {
+        this.setup();
+    }
+    var context = this;
+    this.con.createOffer(function(desc) {
+        context.con.setLocalDescription(desc, function() {});
+        print("Created local offer.", "text-info");
+    }, function () {
+            print("Couldn't create offer", "text-err");
+        }
+    );
+};
+
+RTC.prototype.handleOffer = function(offer) {
+    this.con.setRemoteDescription(offer);
+    var context = this;
+    this.con.createAnswer(function (answer) {
+        print("Created local answer", "text-info");
+        context.con.setLocalDescription(answer);
+    }, function() { 
+            print("Couldn't create answer.", "text-err");
+        }
+    );
+};
+
+RTC.prototype.handleAnswer = function(answer) {
+    this.con.setRemoteDescription(answer);
+};
+
+RTC.prototype.sendMessage = function(text) {
+    var txt = this.cipher.encrypt(text);
+    txt = escape(txt);
+    
+    var niv = forge.random.getBytesSync(32);
+    var nie = escape(this.cipher.encrypt(escape(niv)));
+            
+    this.cipher.iv = niv;
+    
+    var msg = {"user": this.username, "message": txt, "n": nie}
+    
+    var moz = !! navigator.mozGetUserMedia;
+    var data = (moz && msg.file) ? msg.file : JSON.stringify(msg);
+
+    this.dc.send(data);
+};
+
+function getTimestamp() {
+    var totalSec = new Date().getTime() / 1000;
+    var hours = parseInt(totalSec / 3600) % 24;
+    var minutes = parseInt(totalSec / 60) % 60;
+    var seconds = parseInt(totalSec % 60);
+
+    var result = (hours < 10 ? "0" + hours : hours) + ":" +
+                 (minutes < 10 ? "0" + minutes : minutes) + ":" +
+                 (seconds  < 10 ? "0" + seconds : seconds);
+
+    return result;
+};
+
+function print(message, message_type) {
+    document.getElementById("chatlog").innerHTML += "<p class=\"" + message_type + "\">" + "[" + getTimestamp() + "] " + message + "</p>";
+    $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
+};
