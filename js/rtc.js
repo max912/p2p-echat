39ca48056f036426f08faef83f5c845c64f81af5
max <great_max@hotmail.it>
2015-02-19 01:23:31 +0100

    added multichat code (beta)

--- js/rtc.js
+++ js/rtc.js
@@ -5,9 +5,8 @@ var RTC = function() {
 
     this.con = new RTCPeerConnection(cfg, opt);
     this.dc = null;
-    this.desc = null;
-    this.answer;
-    this.who = null;
+    this.id = null;
+    this.rid = null;
     
     this.cipher = new Cipher();
     
@@ -19,47 +18,72 @@ var RTC = function() {
         }
     };
     
-    this.con.onconnection = function() {
-        print("Datachannel connected", "text-info");
-    };
-    
     this.con.ondatachannel = function(evt) {
         context.dc = evt.channel || evt;
         context.dc.onopen = function(evt) {
-            print("Connection estabilished.", "text-info");
-        }
+            printMessage("Connection estabilished.", "text-info");
+            $("#send").prop('disabled', false);
+        };
         context.dc.onmessage = function(evt) {
-            var data = JSON.parse(evt.data);
-            context.parseMessage(data);
+            var request = JSON.parse(evt.data);
+            context.parseRequest(request);
         };
         context.dc.onclose = function(evt) {
-            print("Disconnected.", "text-info");  
+            printMessage("Disconnected.", "text-info");  
         };
     };
     
 };
 
-RTC.prototype.parseMessage = function(data) {
-    this[data.type](data);
+RTC.prototype.parseRequest = function(request) {
+    console.log(request);
+    this[request.type](request);
 };
+
+RTC.prototype.masterNode = function() {
+    this.id = 0;
+    var context = this;
+    try {
+        this.dc = this.con.createDataChannel("test", {"reliable": true});
+
+        this.dc.onopen = function(evt) {
+            printMessage("Connection estabilished.", "text-info");
+            $("#send").prop('disabled', false);
+            var request = new setId(username, null, connections.length - 1);
+            context.dc.send(request.toJString());
+            connectAll(context.rid);
+        }
+        var context = this;
+        this.dc.onmessage = function(evt) {
+            var request = JSON.parse(evt.data);            
+            context.parseRequest(request);
+        };
+        this.dc.onclose = function(evt) {
+            printMessage("Disconnected.", "text-info");  
+        };
+    } catch(e) { 
+            printMessage("Connection error.", "text-err");
+        }
+}
 
 RTC.prototype.setup = function() {
     try {
         this.dc = this.con.createDataChannel("test", {"reliable": true});
 
         this.dc.onopen = function(evt) {
-            print("Connection estabilished.", "text-info");
+            printMessage("Connection estabilished.", "text-info");
+            $("#send").prop('disabled', false);
         }
         var context = this;
         this.dc.onmessage = function(evt) {
-            var data = JSON.parse(evt.data);            
-            context.parseMessage(data);
+            var request = JSON.parse(evt.data);            
+            context.parseRequest(request);
         };
         this.dc.onclose = function(evt) {
-            print("Disconnected.", "text-info");  
+            printMessage("Disconnected.", "text-info");  
         };
     } catch(e) { 
-            print("Connection error.", "text-err");
+            printMessage("Connection error.", "text-err");
         }
 };
 
@@ -71,9 +95,9 @@ RTC.prototype.createLocalOffer = function() {
     this.con.createOffer(function(desc) {
         context.con.setLocalDescription(desc, function() {});
         context.desc = desc;
-        print("Created local offer.", "text-info");
+        printMessage("Created local offer.", "text-info");
     }, function () {
-            print("Couldn't create offer", "text-err");
+            printMessage("Couldn't create offer", "text-err");
         }
     );
 };
@@ -81,12 +105,12 @@ RTC.prototype.createLocalOffer = function() {
 RTC.prototype.handleOffer = function(offer) {
     this.con.setRemoteDescription(offer);
     var context = this;
-    this.con.createAnswer(function (answer) {
-        print("Created local answer", "text-info");
+    this.con.createAnswer(function(answer) {
+        printMessage("Created local answer", "text-info");
         context.con.setLocalDescription(answer);
         context.answer = answer;
     }, function() { 
-            print("Couldn't create answer.", "text-err");
+            printMessage("Couldn't create answer.", "text-err");
         }
     );
 };
@@ -97,120 +121,110 @@ RTC.prototype.handleAnswer = function(answer) {
 
 RTC.prototype.sendMessage = function(text) {
     var txt = this.cipher.encrypt(text);
-    txt = escape(txt);
+    txt = encodeURI(txt);
     
     var niv = forge.random.getBytesSync(32);
-    var nie = escape(this.cipher.encrypt(escape(niv)));
+    var nie = encodeURI(this.cipher.encrypt(encodeURI(niv)));
             
     this.cipher.iv = niv;
     
-    var msg = {"type": "chatMsg", "text": txt, "n": nie, "user": username};
-    
-    var moz = !! navigator.mozGetUserMedia;
-    var data = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-    this.dc.send(data);
+    var request = new chatMsg(username, null, txt, nie);
+    this.dc.send(request.toJString());
 };
 
-RTC.prototype.rGetOffer = function(rid) {
-    var msg = {"type": "getOffer", "text": null, "n": null, "user": username, "rid": 10};
-    
-    var moz = !! navigator.mozGetUserMedia;
-    var data = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-    this.dc.send(data);
+RTC.prototype.rGetOffer = function(id) {
+    var request = new getOffer(id, null, null, null);
+    this.dc.send(request.toJString());
 };
 
-RTC.prototype.rAddOffer = function(offer, mid) {
-    var msg = {"type": "addOffer", "text": offer, "n": null, "user": username, "id": null, "mid": mid};
-    
-    var moz = !! navigator.mozGetUserMedia;
-    var data = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-    this.dc.send(data);
+RTC.prototype.rAddOffer = function(offer, from, to) {
+    var request = new addOffer(from, to, offer);
+    this.dc.send(request.toJString());
 };
 
-RTC.prototype.rAddAnswer = function(answer) {
-    var msg = {"type": "addAnswer", "text": answer, "n": null, "user": username, "id": null, "mid": 1};
-    
-    var moz = !! navigator.mozGetUserMedia;
-    var data = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-    this.dc.send(data);
+RTC.prototype.rAddAnswer = function(answer, from) {
+    var request = new addAnswer(from, null, answer);
+    this.dc.send(request.toJString());
 };
 
-RTC.prototype.chatMsg = function(data) {
-    var msg = data.text;
-    print("<b>" + data.user + ": </b>" + this.cipher.decrypt(unescape(msg)), "message");
+RTC.prototype.chatMsg = function(request) {
+    var msg = request.text;
+    printMessage("<b>" + request.from + ": </b>" + this.cipher.decrypt(decodeURI(msg)), "message");
     
-    var niv = data.n;
-    this.cipher.riv = unescape(this.cipher.decrypt(unescape(niv)));
+    var niv = request.n;
+    this.cipher.riv = decodeURI(this.cipher.decrypt(decodeURI(niv)));
 };
 
-RTC.prototype.getOffer = function(data) {
+RTC.prototype.getOffer = function(request) {
     var cc = new RTC();
-    connections.push(cc);
+    connections[request.from] = cc;
     cc.setup();
-    var rid = data.id;
+    
+    var id = request.from;
     var context = this;
+    
+    cc.con.onicecandidate = function(evt) {
+        if(evt.candidate == null) {
+            var request = new rOffer(context.id, id, JSON.stringify(cc.con.localDescription));
+            context.dc.send(request.toJString());
+        }
+    };
     
     cc.con.createOffer(function(desc) {
         cc.con.setLocalDescription(desc, function() {});
-        var msg = {"type": "rOffer", "text": JSON.stringify(desc), "n": null, "user": username, "rid": rid, "mid": 1};
-        
-        var moz = !! navigator.mozGetUserMedia;
-        var dd = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-        context.dc.send(dd);
-        print("Created local offer.", "text-info");
+        cc.desc = desc;
+        printMessage("Created local offer.", "text-info");
     }, function () {
-            print("Couldn't create offer", "text-err");
+            printMessage("Couldn't create offer", "text-err");
         }
     );
-    
 
 };
 
-
-RTC.prototype.addOffer = function(data) {
+RTC.prototype.addOffer = function(request) {
     var cc = new RTC();
-    connections.push(cc);
+    connections[request.from] = cc;
     
-    var offer = new RTCSessionDescription(JSON.parse(data.text));
+    var offer = new RTCSessionDescription(JSON.parse(request.text));
     cc.con.setRemoteDescription(offer);
     
-    var rid = data.id;
+    var id = request.from;
     var context = this;
     
-    cc.con.createAnswer(function (answer) {
-        print("Created local answer", "text-info");
-        cc.con.setLocalDescription(answer);
-        var msg = {"type": "rAnswer", "text": JSON.stringify(answer), "n": null, "user": username, "rid": rid, "mid": data.mid};
+    cc.con.onicecandidate = function(evt) {
+        if(evt.candidate == null) {
+            var request = new rAnswer(context.id, id, JSON.stringify(cc.con.localDescription));
+            context.dc.send(request.toJString());
+        }
+    };
     
-        var moz = !! navigator.mozGetUserMedia;
-        var dd = (moz && msg.file) ? msg.file : JSON.stringify(msg);
-
-        context.dc.send(dd);
-        
+    cc.con.createAnswer(function(answer) {
+        printMessage("Created local answer", "text-info");
+        cc.con.setLocalDescription(answer);
     }, function() { 
-            print("Couldn't create answer.", "text-err");
+            printMessage("Couldn't create answer.", "text-err");
         }
     );
     
 };
 
-RTC.prototype.addAnswer = function(data) {
-    var cc = connections[1];
-    var answer = new RTCSessionDescription(JSON.parse(data.text));
+RTC.prototype.addAnswer = function(request) {
+    var cc = connections[request.from];
+    var answer = new RTCSessionDescription(JSON.parse(request.text));
     cc.handleAnswer(answer);
 };
 
-RTC.prototype.rOffer = function(data) {
-    var cc = connections[1];
-    cc.rAddOffer(data.text, data.mid);
+RTC.prototype.rOffer = function(request) {
+    var cc = connections[request.to];
+    cc.rAddOffer(request.text,request.from, request.to);
 };
 
-RTC.prototype.rAnswer = function(data) {
-    var cc = connections[data.mid];
-    cc.rAddAnswer(data.text);
+RTC.prototype.rAnswer = function(request) {
+    var cc = connections[request.to];
+    cc.rAddAnswer(request.text, request.from);
 };
+
+RTC.prototype.setId = function(request) {
+    this.id = request.text;
+    username = "User_" + this.id;
+}
