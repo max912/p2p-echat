23c623f9321122d11147ff2a7cea7c8052805f85
TheWorm <theworm@autistici.org>
2015-02-26 17:14:02 +0100

    id changes

--- js/rtc.js
+++ js/rtc.js
@@ -23,6 +23,36 @@ var RTC = function() {
         if(evt.candidate == null) {
             $(".output").val(JSON.stringify(context.con.localDescription));
         }
+        else {
+            var addrs = Object.create(null);
+            var s;
+            addrs["0.0.0.0"] = false;
+            function updateDisplay(newAddr) {
+                if (newAddr in addrs) return;
+                else addrs[newAddr] = true;
+                var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
+                s += displayAddrs.join(" or perhaps ") || "n/a";
+            }
+    
+            function grepSDP(sdp) {
+                var hosts = [];
+                sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+                    if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                        var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                            addr = parts[4],
+                            type = parts[7];
+                        if (type === 'host') updateDisplay(addr);
+                    } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                        var parts = line.split(' '),
+                            addr = parts[2];
+                        updateDisplay(addr);
+                    }
+                });
+            }
+            grepSDP("a=" + evt.candidate.candidate);
+            console.log(s);
+            this.id = evt.candidate.candidate.match(/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\s+[0-9]+/g);
+        }
     };
     
     this.con.ondatachannel = function(evt) {
